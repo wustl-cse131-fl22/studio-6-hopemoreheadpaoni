75fd3218694730bd0f5d0c38ac97bf28eef8dbd8
benpaoni <benpaoni@bens-mbp.wusm-wifi.wucon.wustl.edu>
2022-10-26 15:49:40 -0500

    studio 6

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,10 +12,13 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
+			if (n == 0) {
+				return 0;
+			}
+			else {
+				return 1/Math.pow(2,n) + geometricSum(n-1);
+			}
 			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
-		
 	}
 
 	/**
@@ -27,10 +30,13 @@ public class RecursiveMethods {
 	 * @return greatest common divisor of p and q
 	 */
 	public static int gcd(int p, int q) {
-		
+			if (q == 0) {
+				return p;
+			}
+			else {
+				return gcd(q, p % q);
+			}
 			// FIXME compute the gcd of p and q using recursion
-			return 0;
-		
 	}
 
 	
@@ -42,10 +48,26 @@ public class RecursiveMethods {
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
-		
+			int[] copyArray = new int[array.length];
+			for (int i = 0; i < array.length; i++) {
+				copyArray[i] = array[i];
+			}
+			
 			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
+			return helper (copyArray, 0);
 		
+	}
+	
+	public static int[] helper(int[] array, int index) {
+		if (index == array.length / 2) {
+			return array;
+		}
+		else {
+			int temp = array[index];
+			array[index] = array[array.length-index-1];
+			array[array.length-index-1] = temp;
+			return helper(array, index + 1);
+		}
 	}
 
 	/**
